e85a8ab3fc0e29b49fa602ed17a1a97d895d37b5
Carl Csaposs <carl.csaposs@canonical.com>
2023-11-03 11:21:02 +0000

    Download lock file

--- cli/cli/build.py
+++ cli/cli/build.py
@@ -1,12 +1,17 @@
 import argparse
 import json
 
+import requests
+
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("charms_file")
+    parser.add_argument("charm_locks_json")
     args = parser.parse_args()
-    with open(args.charms_file, "r") as file:
-        charms = json.load(file)
-    for charm in charms:
-        print(charm)
+    with open(args.charm_locks_json, "r") as file:
+        charm_lock_files = json.load(file)
+    for url in charm_lock_files:
+        response = requests.get(url)
+        response.raise_for_status()
+        lock_file = response.text
+        print(lock_file)
