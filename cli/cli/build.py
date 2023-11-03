104bb32675db86284b57cf77f9209c09d0e3a140
Carl Csaposs <carl.csaposs@canonical.com>
2023-11-03 10:55:39 +0000

    Parse charms.json

--- cli/cli/build.py
+++ cli/cli/build.py
@@ -1,2 +1,12 @@
+import argparse
+import json
+
+
 def main():
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument("charms_file")
+    args = parser.parse_args()
+    with open(args.charms_file, "r") as file:
+        charms = json.load(file)
+    for charm in charms:
+        print(charm)
